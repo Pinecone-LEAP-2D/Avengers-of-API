03ff80019cc86fa4aa8c2dd1b853082c61277c7f
24LP4621 24LP4621 <24LP4621@G301-CC0210.local>
2025-04-08 18:52:27 +0800

    navbaradded

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -1,15 +1,15 @@
 import Header from "@/components/Header";
 import HomeMenu from "./_components/HomeMenu";
+import NavBar from "@/components/NavBar";
 
-
-export default function Home(){
-    return(
-        <div className="flex flex-col items-center">
-            <Header />
-            <div className="flex mt-[60px]">
-                {/* <NavigationBar /> */}
-                <HomeMenu />
-            </div>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <Header />
+      <div className="flex mt-[60px] gap-[300px]">
+        <NavBar location={"home"} />
+        <HomeMenu />
+      </div>
+    </div>
+  );
 }
