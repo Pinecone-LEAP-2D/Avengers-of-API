19c0982e798fdb49d2461de9e61f98bb3ee9897c
Zen1e <monkhdelger.edts@gmail.com>
2025-04-11 16:31:50 +0800

    login

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import LoginForm from "./_components/LoginForm";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  useEffect(()=>{
+    if(typeof window !== undefined){
+      window.localStorage.clear();
+    }
+  },[])
   return (
     <div>
       <LoginForm />
