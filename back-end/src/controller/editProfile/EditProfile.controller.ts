f1579eedcc7ea0bbe1713368eb3f22a871028890
borisinparis <mnkymgaa@gmail>
2025-04-14 18:03:32 +0800

    232324

---
+++ back-end/src/controller/editProfile/EditProfile.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import prisma from "../../../prismaClient";
+import bcrypt from "bcryptjs"; // If password needs hashing
+
+export const EditProfileController = async (req: Request, res: Response) => {
+  const {
+    name,
+    about,
+    avatarImage,
+    socialMediaURL,
+    successMessage,
+    userId,
+    password,
+    bankCard,
+  } = req.body;
+
+  try {
+    const updatedProfile = await prisma.profile.update({
+      where: { userId },
+      data: {
+        name,
+        about,
+        avatarImage,
+        socialMediaURL,
+        successMessage,
+        user: password
+          ? {
+              update: {
+                password: await bcrypt.hash(password, 10),
+              },
+            }
+          : undefined,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    let updatedCard = null;
+    if (bankCard?.id) {
+      updatedCard = await prisma.bankCard.update({
+        where: { id: bankCard.id },
+        data: {
+          country: bankCard.country,
+          firstName: bankCard.firstName,
+          lastName: bankCard.lastName,
+          cardNumber: bankCard.cardNumber,
+          expiryDate: new Date(bankCard.expiryDate),
+        },
+      });
+    }
+
+    res.status(200).json({
+      profile: updatedProfile,
+      bankCard: updatedCard,
+    });
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    res.status(500).json({ error: "Failed to update profile" });
+  }
+};
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -6,6 +6,7 @@ import { ProfileRouter } from "./routers/ProfileRouter";
 import dotenv from "dotenv";
 import { BankRouter } from "./routers/BankCardRouter";
 import { DonationRouter } from "./routers/DonationRouter";
+import { EditProfileRouter } from "./routers/EditProfileRouter";
 dotenv.config();
 
 const app = express();
@@ -24,6 +25,7 @@ app.use("/user", LoginUserRouter);
 app.use("/profile", ProfileRouter);
 app.use("/bank", BankRouter);
 app.use("/donation", DonationRouter);
+app.use("/profile", EditProfileRouter);
 
 app.listen(port, () => {
   console.log("server start at: http://localhost/3000");
